40184a6ec73ad86693a5a15b10df755ff8aaa27a
Nivethitha SoundarRajan <52025677+Nivethitha167@users.noreply.github.com>
2020-11-28 17:13:07 +0530

    Add files via upload

---
+++ web scrap.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+
+site = 'https://news.google.com/news/rss'
+op = urlopen(site) #Open that site
+rd = op.read() #read data from site
+op.close()   # close the object
+sp_page = soup(rd,'xml') #scrapping data from site
+#print(sp_page)
+news_list = sp_page.find_all('item') #finding news
+for news in news_list:  #printing news
+    print(news.title.text)
+    print(news.link.text)
+    print(news.pubDate.text)
+    print('-'*60)
+    print("\n")
